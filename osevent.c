6b80cb32a26a4c54a90d32caf974526b6cc57d80
kwangzhu <21540729@student.uwa.edu.au>
2016-09-12 21:53:56 +0800

    Project 1

--- osevent.c
+++ osevent.c
@@ -1,1 +1,19 @@
-CITS2002
+/* CITS2002 Project 1 2016
+ Names: Samir Vojdani, Kwang Zhu Ong
+ Student numbers: 21567881, 21540729
+ Date: date-of-submission
+ */
+
+//  Compile with: cc -std=c99 -Wall -Werror -pedantic -o osevent osevent.c
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main (void) {
+    
+    printf("1234");
+}
